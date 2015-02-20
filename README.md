aea0f4d9043cf05af52a88d54a6d640d1726b142
Brian Broll <brian.broll@gmail.com>
2015-02-20 10:34:47 -0600

    Added Readme info and misc comments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # BasicCalculationApp
+
+The calculation apps in the Homeward Bound app suite allow for detections of iBeacons to be combined (using techniques such as triangulation) to effectively pinpoint the location of the given iBeacon.
+
+This is app is the most basic calculation app - it simply returns the measurement that it received and does not perform any optimizations of the measurement.
+
+Currently, this app can be run by simply executing 
+
+    python app.py
--- app.py
+++ app.py
@@ -10,7 +10,6 @@ storeAddress = "tcp://*:2002";
 subscribeAddress = "tcp://127.0.0.1:2001";
 subscribeTopic = "calculation_v"+str(version);
 storeHeader = "StoreMeasurement";
-headerLen = len(subscribeTopic);
 
 # Create publish socket
 context = zmq.Context();
@@ -24,7 +23,7 @@ subSocket.connect(subscribeAddress);
 subSocket.setsockopt_string(zmq.SUBSCRIBE, subscribeTopic);
 
 def createResponse(msg):
-    start = msg.index(subscribeTopic)+headerLen;
+    start = msg.index('{');
     print("Measurement extracted: "+msg[start:]);
     return storeHeader+msg[start:];
 
@@ -34,12 +33,11 @@ try:
         msgs = subSocket.recv_multipart();
         for msg in msgs:
             print('Received msg: '+str(msg));
+            # Create response message
             resp = createResponse(str(msg));
+            # Send response message
             pubSocket.send_string(resp);
-        # Create response message
 except KeyboardInterrupt:
     pass
-
-   
 
 print("Exiting...");
